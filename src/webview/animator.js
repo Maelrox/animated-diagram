a4212af4c0198efc9422a56353b53766bfaee7b9
Maelrox <oscarquiroz@gmail.com>
2025-02-21 06:24:56 -0500

    fixing animator update order out of sync

--- src/webview/animator.js
+++ src/webview/animator.js
@@ -1,69 +1,55 @@
+// Create an animated particle (svg circle) that follows the connection path
 function addFlowAnimation(connection) {
     const particle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    particle.classList.add('flow-particle');
-    particle.setAttribute('r', '4');
-    
+    const path = connection.querySelector('path');
     const animateMotion = document.createElementNS('http://www.w3.org/2000/svg', 'animateMotion');
     animateMotion.setAttribute('dur', '2s');
-    
-    const path = connection.querySelector('path');
     animateMotion.setAttribute('path', path.getAttribute('d'));
-    
+    particle.classList.add('flow-particle');
+    particle.setAttribute('r', '4');
     particle.appendChild(animateMotion);
     connection.appendChild(particle);
 }
 
-function stopAnimations() {
-    connections.forEach(conn => {
-        const particle = conn.element.querySelector('.flow-particle');
-        if (particle) {
-            const oldParticle = particle;
-            const newParticle = oldParticle.cloneNode(true);
-            oldParticle.parentNode.replaceChild(newParticle, oldParticle);
-        }
-    });
+function loopAnimations() {
+    const connectionsByOrder = groupConnectionsByOrder(connections);
+    const uniqueOrders = [...connectionsByOrder.keys()].sort((a, b) => a - b);
+    animateOrders(uniqueOrders, connectionsByOrder, 0);
 }
 
-function loopAnimations() {
+function animateOrders(uniqueOrders, connectionsByOrder, orderIndex) {
+    if (orderIndex >= uniqueOrders.length) {
+        requestAnimationFrame(loopAnimations);
+        return;
+    }
+
+    const currentOrder = uniqueOrders[orderIndex];
+    const currentConnections = connectionsByOrder.get(currentOrder);
+
+    currentConnections.forEach(conn => {
+        const animateMotion = conn.element.querySelector('animateMotion');
+        if (animateMotion) {
+            animateMotion.setAttribute('repeatCount', '1');
+            animateMotion.setAttribute('begin', '0s');
+            animateMotion.beginElement();
+        }
+    });
+
+    setTimeout(() => animateOrders(uniqueOrders, connectionsByOrder, orderIndex + 1), 2000);
+}
+
+function groupConnectionsByOrder(connections) {
     const connectionsByOrder = new Map();
     connections.forEach(conn => {
-        const order = conn.order;
-        if (!connectionsByOrder.has(order)) {
-            connectionsByOrder.set(order, []);
+        if (!connectionsByOrder.has(conn.order)) {
+            connectionsByOrder.set(conn.order, []);
         }
-        connectionsByOrder.get(order).push(conn);
+        connectionsByOrder.get(conn.order).push(conn);
     });
-    
-    const uniqueOrders = [...connectionsByOrder.keys()].sort((a, b) => a - b);
-    let orderIndex = 0;
-    
-    function animateOrder() {
-        if (orderIndex >= uniqueOrders.length) {
-            orderIndex = 0;
-            requestAnimationFrame(loopAnimations);
-            return;
-        }
-        
-        const currentOrder = uniqueOrders[orderIndex];
-        const currentConnections = connectionsByOrder.get(currentOrder);
-        
-        currentConnections.forEach(conn => {
-            const animateMotion = conn.element.querySelector('animateMotion');
-            if (animateMotion) {
-                animateMotion.setAttribute('repeatCount', '1');
-                animateMotion.setAttribute('begin', '0s');
-                animateMotion.beginElement();
-            }
-        });
-        
-        orderIndex++;
-        // 2 seconds per group, not per individual animation
-        setTimeout(animateOrder, 2000);
-    }
-    
-    animateOrder();
+    return connectionsByOrder;
 }
 
+// Reverse the direction of the particle
 function updateAnimationDirection(connection) {
     const particle = connection.querySelector('.flow-particle');
     const animateMotion = particle.querySelector('animateMotion');
